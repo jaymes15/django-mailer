704bb40b611f1908959cc6f42c199c4b36ae1ea2
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2019-12-04 23:46:28 -0800

    Forgot password custom template

--- signup/ajaxsignup/urls.py
+++ signup/ajaxsignup/urls.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.conf.urls import url
 from django.urls import path, include
 from . import views
@@ -13,6 +13,10 @@ urlpatterns = [
     path('signin/', views.firstlogin,name='firstlogin'),
     path('login/', views.login_view,name='login'),
     path('accounts/login/', views.login_view,name='login'),
-    path('logout/', views.logout_view,name='logout')
+    path('logout/', views.logout_view,name='logout'),
+    url('^reset-password/$', PasswordResetView.as_view(), name='reset_password'),
+    url('^reset-password/done/$', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('password/done',PasswordResetCompleteView.as_view(),name="password_reset_complete"),
     
 ]
--- signup/signup/settings.py
+++ signup/signup/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ajaxsigniing',
+    'ajaxsignup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ajaxsigniing',
-    'ajaxsignup',
+    
 ]
 
 MIDDLEWARE = [
